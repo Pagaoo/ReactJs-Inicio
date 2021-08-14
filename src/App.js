911ea23ac36ed23775141753e9fa4958783dfb9a
Gabriel Paganini <gabrielpaganinibarroso@hotmail.com>
2021-08-14 08:54:45 -0300

    requisição de api com axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header.js';
 import Body from './components/Body.js';
+import Api from './components/Api.js';
 
 function App() {
+
+  const [nomes, setNomes] = useState([{}]);
+
+  async function initApi(){
+    let pegaNomes = await Api.getPersons();
+    setNomes(pegaNomes);
+  }
+
+  useEffect(() => {
+    initApi();
+  }, []);
+
   return (
     <div className="App">
       
       <Header />
       <Body />
+
+    {
+      nomes.map(function(data){
+        return(
+          <div>
+            <h2>{data.name} | {data.email}</h2>
+          </div>
+        )
+      })
+    }
+
     </div>
   );
 }
